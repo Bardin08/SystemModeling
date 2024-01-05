40f84ff0a54bbca30d8eee098fee9ca5bda7100c
Bardin08 <vbardin810@gmail.com>
2024-01-05 11:23:07 +0200

    Implement routing policy for lab3
    Signed-off-by: Bardin08 <vbardin810@gmail.com>

---
+++ SystemModeling/Lab3/Extensions/QueueExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace SystemModeling.Lab3.Extensions;
+
+public static class QueueExtensions
+{
+    public static async Task TransferLastItem<T>(
+        this Channel<T> src,
+        ChannelWriter<T> dest,
+        CancellationToken ct)
+    {
+        T lastItem = default!;
+
+        var amount = src.Reader.Count;
+        await foreach (var item in src.Reader.ReadAllAsync(ct))
+        {
+            amount--;
+            if (amount is 0)
+            {
+                lastItem = item;
+                break;
+            }
+
+            await src.Writer.WriteAsync(item, ct);
+        }
+
+        await dest.WriteAsync(lastItem, ct);
+    }
+}
