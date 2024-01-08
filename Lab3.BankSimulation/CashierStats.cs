0b0704d6345869369444ee880756ad02fe34f4e9
Bardin08 <vbardin810@gmail.com>
2024-01-08 23:49:30 +0200

    Add base stats provider
    Signed-off-by: Bardin08 <vbardin810@gmail.com>

---
+++ Lab3.BankSimulation/CashierStats.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab3.BankSimulation;
+
+public record CashierStats(
+    int TotalServed,
+    int TotalRejected,
+    double TotalServingTime,
+    double TotalWaitingTime,
+    int TotalQueueSwap)
+{
+    public double AverageLoadPerCashier => TotalServingTime / TotalServed;
+    public double AverageCustomerStayTime => (TotalServingTime + TotalWaitingTime) / TotalServed;
+    public double PercentageOfRejectedCustomers => (double)TotalRejected / TotalServed * 100;
+    public int TotalQueueSwaps => TotalQueueSwap;
+
+    public double AverageQueueLength => -1;
+}
