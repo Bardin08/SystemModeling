58bcee1ab3247f9aef17fbd17ad740660e222996
Bardin08 <vbardin810@gmail.com>
2024-01-18 02:37:56 +0200

    Add lab03 project file
    Signed-off-by: Bardin08 <vbardin810@gmail.com>

---
+++ Lab3.Hospital/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lab3.Hospital;
+
+const double timeThreshold = 1000;
+
+var hospital = new Hospital();
+
+while (hospital.GlobalTime < timeThreshold)
+{
+    hospital.GenerateNewPatients();
+    hospital.ProcessPatients();
+}
+
+hospital.PrintStatistics();
+
+Console.WriteLine("Completed!");
